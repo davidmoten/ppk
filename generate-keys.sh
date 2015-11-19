2f3109eb8e1a15997a4285004bfe7e50e9e66371
Dave Moten <davidmoten@gmail.com>
2015-11-19 17:11:48 +1100

    create keys in target directory

--- generate-keys.sh
+++ generate-keys.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-set -x
+# set -x
 set -e
-openssl genrsa -out keypair.pem 2048
-openssl rsa -in keypair.pem -outform DER -pubout -out public.der
-openssl pkcs8 -topk8 -nocrypt -in keypair.pem -outform DER -out private.der
-rm keypair.pem
+mkdir -p target
+openssl genrsa -out target/keypair.pem 2048
+openssl rsa -in target/keypair.pem -outform DER -pubout -out target/public.der
+openssl pkcs8 -topk8 -nocrypt -in target/keypair.pem -outform DER -out target/private.der
+rm target/keypair.pem
+echo "created public.der and private.der in target directory"
